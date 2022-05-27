981dcdc475ab318bc7382d2df71440db7e00d55b
shiauweitsung <zz830527@gmail.com>
2022-05-27 17:03:46 +0800

    [change] import account use json ui page update

--- ui/pages/create-account/import-account/json.js
+++ ui/pages/create-account/import-account/json.js
@@ -10,7 +10,7 @@ import Button from '../../../components/ui/button';
 import { getMostRecentOverviewPage } from '../../../ducks/history/history';
 
 const HELP_LINK =
-  'https://metamask.zendesk.com/hc/en-us/articles/360015489331-Importing-an-Account';
+  'https://alphacarbon.network/';
 
 class JsonImportSubview extends Component {
   state = {
@@ -57,16 +57,14 @@ class JsonImportSubview extends Component {
         />
         <div className="new-account-create-form__buttons">
           <Button
-            type="secondary"
-            large
+            type="cancel"
             className="new-account-create-form__button"
             onClick={() => history.push(mostRecentOverviewPage)}
           >
             {this.context.t('cancel')}
           </Button>
           <Button
-            type="primary"
-            large
+            type="primaryGradient"
             className="new-account-create-form__button"
             onClick={() => this.createNewKeychain()}
             disabled={!enabled}
